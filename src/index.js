1ac33d4e1824f997a170c41b83ebd5e1b61105c3
RumitAshara <126247287+RumitAshara@users.noreply.github.com>
2025-09-20 22:25:10 +0530

    DB CONNECTION

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from 'dotenv';
+import express from "express";
+import connectDB from "./db/index.js";
+dotenv.config({
+  path: './env'
+});
+//second apporch
+connectDB();
+
+
+
+//first apporch to connect database
+
+// (async()=>{
+//         try{
+//           await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//           app.on("error",(error)=>{
+//             console.log("ERRR",error);
+//             throw error;
+//           });
+
+//           app.listen(process.env.PORT,()=>{
+//             console.log("APP LISTENING ON PORT",process.env.PORT);
+//           });
+//         }catch(error){
+
+//             console.error("ERROR: ",error);
+//             throw err;
+//         }
+// })()
